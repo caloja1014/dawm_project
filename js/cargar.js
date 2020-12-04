5185007ab9d729b8f6fcd06558337fd22b276f47
caloja1014 <caloja@espol.edu.ec>
2020-12-04 00:28:35 -0500

    Adiccion de barra de busqueda en shop

--- js/cargar.js
+++ js/cargar.js
@@ -5,6 +5,8 @@ let cargarJSON = () =>{
         return data.json()
     })
     .then((productos)=>{
+
+
         json = productos;
         let carousel = document.getElementById("carousel-products");
         let firstCateg = productos[0];
@@ -54,7 +56,11 @@ let cargarJSON = () =>{
                     '</div>'                   
             }
         }
-    
+        let divCategoria = document.createElement("div");
+        divCategoria.id = "divBusqueda";
+        divCategoria.className = "row";
+        divCategoria.style.display = "none";
+        divproductos.appendChild(divCategoria); 
         
     }).then(()=>{
         onclicks();
@@ -67,9 +73,65 @@ let cargarJSON = () =>{
     })
 }
 
+
+
 document.addEventListener('DOMContentLoaded', ()=> {
     cargarJSON();
 });
+
+
+var buscador = document.getElementById("inputProducto")
+
+buscador.oninput=()=>{
+    let value =buscador.value;
+    let categorias = document.getElementById("categorias");
+    let catSelect=categorias.getElementsByClassName("categ-seleccionada");
+    
+    let divproductos = document.getElementById("productos");
+    let divBusqueda= document.getElementById("divBusqueda");
+    if(value==""){
+        let idDiv=catSelect[0].id;
+        document.getElementById("div"+idDiv).style.display="";
+        divBusqueda.innerHTML="";
+    }
+    else{
+        let productos=divproductos.getElementsByClassName("row");
+        for (let p of productos){
+            p.style.display="none"
+        }
+        divBusqueda.innerHTML="";
+        divBusqueda.style.display = "";
+        for (let elemento of json){
+            for (let producto of elemento.productos){
+                if (producto.nombre.includes(value)){
+
+                    divBusqueda.innerHTML += 
+                    '<div class="col-lg-4 col-md-6 mb-4">'+
+                        '<div class="card h-100">'+
+                            '<a href="#"><img class="card-img-top" src=./assets/productos/'+elemento.categoria+'/'+producto.img +' alt=""></a>'+
+                            '<div class="card-body">'+
+                                '<div class="card-top">'+
+                                    '<h4 class="card-title">'+
+                                        '<a href="#">'+producto.nombre+'</a>'+
+                                    '</h4>'+
+                                    '<h5>$'+producto.precio+'</h5>'+
+                                    '<p class="card-text">'+producto.descripcion+'</p>'+
+                                '</div>'+
+                                '<a class="button btn-comprar">Comprar</a>'
+                            '</div>'+
+                        '</div>'+
+                    '</div>' 
+                }                  
+                
+
+            }
+
+        }
+    }
+    
+}
+
+
 
 function comprar(){
     for(let item of json){
