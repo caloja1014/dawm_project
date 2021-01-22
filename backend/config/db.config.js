79e0f3e20fba81b153831d7319dc34af52cdc9e8
caloja1014 <caloja@espol.edu.ec>
2021-01-22 00:56:34 -0500

    usuario model

---
+++ backend/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "104.154.47.206",
+    USER: "root",
+    PASSWORD: "dawm",
+    DB: "meraki",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
