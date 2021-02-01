484c581fccde6982d6f513a009db7b352c1e0676
JaimePizarro <japibaro@espol.edu.ec>
2021-01-31 19:10:13 -0500

    admin login and auth

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const diasController= require("../controllers/nodb/dias.controller");
 const noticiasController= require("../controllers/nodb/noticias.controller");
 const mesesController=require("../controllers/nodb/meses.controller")
+const adminController = require("../controllers/db/admin.controller");
 //router.get("/",noticiasController.findAll);
 
 
@@ -20,6 +21,6 @@ router.get("/ventaCateg/:categ",mesesController.ventaAnualPorCateg);
 
 router.post("/addNew",noticiasController.create);
 
-
+router.post("/login",adminController.loginAdmin);
 
 module.exports = router;
